c5eec37a57512b5e74850a293ccff96412186119
msx47 <mohitsrivastava67@gmail.com>
2021-10-16 14:45:20 +0530

    added: method to allow user to generate a tree node

--- src/core/parseTree.ts
+++ src/core/parseTree.ts
@@ -1,4 +1,4 @@
-import { HtmlAttributeType, ParseTreeInitializer } from "../types/utilTypes";
+import { HtmlAttributeType, ParseTreeInitializer } from '../types/utilTypes';
 
 class ParseTree {
   type: string | undefined;
@@ -11,14 +11,18 @@ class ParseTree {
 
   constructor(init?: ParseTreeInitializer) {
     init = init || {};
-    this.type = init.type || "";
+    this.type = init.type || '';
     this.classes = init.classes || [];
-    this.id = init.id || "";
+    this.id = init.id || '';
     this.attributes = init.attributes || [];
-    this.text = init.text || "";
+    this.text = init.text || '';
     this.children = init.children || [];
     this.isTextNode = init.isTextNode || false;
   }
+}
+
+export function generateParseTree(options?: ParseTreeInitializer) {
+  return new ParseTree(options);
 }
 
 export default ParseTree;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import astToHtml from './core/codegen';
 import parser from './core/parser';
+import { generateParseTree } from './core/parseTree';
 
-export { parser, astToHtml };
+export { parser, astToHtml, generateParseTree };
